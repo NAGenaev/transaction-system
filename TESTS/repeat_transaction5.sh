3d03d65792a01c48d1f569769204fde70c5efc53
Genaev <genaevlive@gmail.com>
2025-03-28 14:19:02 +0300

    remaked

---
+++ TESTS/repeat_transaction5.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Параметры для транзакции
+URL="http://localhost:8000/transactions"
+AMOUNT="1000"
+SENDER="123e4567-e89b-12d3-a456-426614174008"
+RECIPIENT="123e4567-e89b-12d3-a456-426614174009"
+
+# Повторить транзакцию 10000 раз
+for i in {1..10000}
+do
+  echo "Отправка транзакции №$i"
+  curl -X 'POST' \
+    "$URL" \
+    -H 'Content-Type: application/json' \
+    -d "{
+      \"amount\": \"$AMOUNT\",
+      \"sender\": \"$SENDER\",
+      \"recipient\": \"$RECIPIENT\"
+    }"
+  echo -e "\n---\n"
+done
